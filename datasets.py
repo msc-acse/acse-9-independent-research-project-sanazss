f98b0e563227b9929779eab5b111f22bf7aff063
sanazss <43985794+sanazss@users.noreply.github.com>
2019-08-30 10:36:22 +0100

    reading and loading data

---
+++ datasets.py
@@ -0,0 +1,295 @@
+import glob
+import math
+import os
+import random
+import shutil
+from pathlib import Path
+
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from utils import xyxy2xywh, xywh2xyxy
+
+img_formats = ['.tif'] #image format
+
+class LoadImages:  # for inference
+    def __init__(self, path, img_size=256):
+        self.height = img_size #image size is 256 or 512 for our experiments
+
+        files = []
+        if os.path.isdir(path):
+            files = sorted(glob.glob('%s/*.*' % path))
+        elif os.path.isfile(path):
+            files = [path]
+
+        images = [x for x in files if os.path.splitext(x)[-1].lower() in img_formats] #finding images
+        nI = len(images) #number of images
+        self.img_size = img_size #imag eisze
+        self.files = images 
+        self.nF = nI  # number of files
+        assert self.nF > 0, 'No images or videos found in ' + path
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.nF:
+            raise StopIteration
+        path = self.files[self.count]
+
+        # Read image
+        self.count += 1
+        img0 = cv2.imread(path, cv2.IMREAD_ANYDEPTH|cv2.IMREAD_UNCHANGED)
+		
+        #stacking channels   
+        img0 = np.stack((img0,)*3, axis =2)# BGR
+        assert img0 is not None, 'File Not Found ' + path
+        print('image %g/%g %s: ' % (self.count, self.nF, path), end='')
+
+        # Padded resize-usefull for augmentation- we want to keep the square shape of the image
+        img, *_ = letterbox(img0, new_shape=self.img_size)
+
+        # Normalize RGB
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
+        img = np.ascontiguousarray(img, dtype=np.float32)  # uint8 to float32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+        return path, img, img0
+
+    def __len__(self):
+        return self.nF  # number of files
+
+
+class LoadImagesAndLabels(Dataset):  # for training/testing
+    def __init__(self, path, img_size=256, batch_size=16, augment=False,hyp = None):
+        with open(path, 'r') as f: #opening image path
+            self.img_files = [x for x in f.read().splitlines() if os.path.splitext(x)[-1].lower() in img_formats]
+
+        n = len(self.img_files)
+        bi = np.floor(np.arange(n) / batch_size).astype(np.int)  # batch index
+        nb = bi[-1] + 1  # number of batches
+        assert n > 0, 'No images found in %s' % path
+
+        self.n = n
+        self.batch = bi  # batch index of image
+        self.img_size = img_size
+        self.augment = augment
+        self.hyp = hyp
+
+        # Define labels
+        self.label_files = [x.replace('images', 'labels').replace(os.path.splitext(x)[-1], '.txt') #define labels by replacing image folder to label forlder
+                            for x in self.img_files]
+
+        self.imgs = [None] * n
+        self.labels = [None] * n
+        
+        # Detect corrupted images 
+        detect_corrupted_images = False
+        if detect_corrupted_images:
+            from skimage import io  
+            for file in tqdm(self.img_files, desc='Detecting corrupted images'):
+                try:
+                    _ = io.imread(file)
+                except:
+                    print('Corrupted image detected: %s' % file)
+
+    def __len__(self):
+        return len(self.img_files)
+		
+    def __getitem__(self, index):
+        img_path = self.img_files[index] #image path
+        label_path = self.label_files[index] #label path
+        hyp = self.hyp
+
+        # Load image
+        img = self.imgs[index]
+        if img is None:
+            img = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH|cv2.IMREAD_UNCHANGED) #reading image with OpenCV
+            #stacking channels
+            img = np.stack((img,)*3, axis =2)  # BGR
+            assert img is not None, 'File Not Found ' + img_path
+			
+			#augmentation
+            r = self.img_size / max(img.shape)
+            if self.augment and r < 1:  # if training (NOT testing), downsize to inference shape
+                h, w, _ = img.shape
+                img = cv2.resize(img, (int(w * r), int(h * r)), interpolation=cv2.INTER_AREA)
+
+            if self.n < 3000:  # cache into memory if image count < 3000
+                self.imgs[index] = img
+            
+
+        # Letterbox
+        h, w, _ = img.shape #image shape
+        shape = self.img_size
+        img, ratiow, ratioh, padw, padh = letterbox(img, new_shape=shape, mode='square') #image and its padding after applying letterbox function
+            
+        # Load labels
+        labels = []
+        if os.path.isfile(label_path):
+            x = self.labels[index]
+            if x is None:  # labels not preloaded
+                with open(label_path, 'r') as f:
+                    x = np.array([x.split() for x in f.read().splitlines()], dtype=np.float32)
+                    self.labels[index] = x  # save for next time
+
+            if x.size > 0:
+                # Normalized xywh to pixel xyxy format
+                labels = x.copy()
+                labels[:, 1] = ratiow * w * (x[:, 1] - x[:, 3] / 2) + padw
+                labels[:, 2] = ratioh * h * (x[:, 2] - x[:, 4] / 2) + padh
+                labels[:, 3] = ratiow * w * (x[:, 1] + x[:, 3] / 2) + padw
+                labels[:, 4] = ratioh * h * (x[:, 2] + x[:, 4] / 2) + padh
+
+        # Augment image and labels
+        if self.augment:
+            img, labels = random_affine(img, labels,
+                                        degrees=hyp['degrees'],
+                                        translate=hyp['translate'],
+                                        scale=hyp['scale'],
+                                        shear=hyp['shear'])
+
+        nL = len(labels)  # number of labels
+        if nL:
+            # convert xyxy to xywh
+            labels[:, 1:5] = xyxy2xywh(labels[:, 1:5])
+
+            # Normalize coordinates 0 - 1
+            labels[:, [2, 4]] /= img.shape[0]  # height
+            labels[:, [1, 3]] /= img.shape[1]  # width
+
+        if self.augment:
+            # random left-right flip
+            lr_flip = True
+            if lr_flip and random.random() > 0.5:
+                img = np.fliplr(img)
+                if nL:
+                    labels[:, 1] = 1 - labels[:, 1]
+
+            # random up-down flip
+            ud_flip = False
+            if ud_flip and random.random() > 0.5:
+                img = np.flipud(img)
+                if nL:
+                    labels[:, 2] = 1 - labels[:, 2]
+
+        labels_out = torch.zeros((nL, 6))
+        if nL:
+            labels_out[:, 1:] = torch.from_numpy(labels)
+
+        # Normalize
+        
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img, dtype=np.float32)  # uint8 to float32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+        return torch.from_numpy(img), labels_out, img_path, (h, w)
+
+    @staticmethod
+    def collate_fn(batch):
+        img, label, path, hw = list(zip(*batch))  # transposed
+        for i, l in enumerate(label):
+            l[:, 0] = i  # add target image index for build_targets()
+        return torch.stack(img, 0), torch.cat(label, 0), path, hw
+
+
+def letterbox(img, new_shape=256, color=(128, 128, 128), mode='auto'):
+    # Resize a rectangular image to a 32 pixel multiple rectangle
+    # is applied for augmentation when we need to fill blank space
+    shape = img.shape[:2]  # current shape [height, width]
+
+    if isinstance(new_shape, int):
+        ratio = float(new_shape) / max(shape)
+    else:
+        ratio = max(new_shape) / max(shape)  # ratio  = new / old
+    ratiow, ratioh = ratio, ratio
+    new_unpad = (int(round(shape[1] * ratio)), int(round(shape[0] * ratio)))
+
+    # Compute padding 
+    if mode is 'auto':  # minimum rectangle
+        dw = np.mod(new_shape - new_unpad[0], 32) / 2  # width padding
+        dh = np.mod(new_shape - new_unpad[1], 32) / 2  # height padding
+    elif mode is 'square':  # square
+        dw = (new_shape - new_unpad[0]) / 2  # width padding
+        dh = (new_shape - new_unpad[1]) / 2  # height padding
+    elif mode is 'rect':  # if there is rectangualr image
+        dw = (new_shape[1] - new_unpad[0]) / 2  # width padding
+        dh = (new_shape[0] - new_unpad[1]) / 2  # height padding
+    elif mode is 'scaleFill':
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape, new_shape)
+        ratiow, ratioh = new_shape / shape[1], new_shape / shape[0]
+        
+    if shape[::-1] != new_unpad:
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_AREA)  # resize
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # padded square
+    return img, ratiow, ratioh, dw, dh
+
+
+def random_affine(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10):
+    # torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(.1, .1), scale=(.9, 1.1), shear=(-10, 10))
+    # https://medium.com/uruvideo/dataset-augmentation-with-random-homographies-a8f4b44830d4
+
+    if targets is None:
+        targets = []
+    border = 0  # width of added border (optional)
+    height = img.shape[0] + border * 2
+    width = img.shape[1] + border * 2
+
+    # Rotation and Scale
+    R = np.eye(3)
+    a = random.uniform(-degrees, degrees)
+    s = random.uniform(1 - scale, 1 + scale)
+    R[:2] = cv2.getRotationMatrix2D(angle=a, center=(img.shape[1] / 2, img.shape[0] / 2), scale=s)
+
+    # Translation
+    T = np.eye(3)
+    T[0, 2] = random.uniform(-translate, translate) * img.shape[0] + border  # x translation (pixels)
+    T[1, 2] = random.uniform(-translate, translate) * img.shape[1] + border  # y translation (pixels)
+
+    # Shear
+    S = np.eye(3)
+    S[0, 1] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # x shear (deg)
+    S[1, 0] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # y shear (deg)
+
+    M = S @ T @ R  # Combined rotation matrix. ORDER IS IMPORTANT HERE!!
+    imw = cv2.warpAffine(img, M[:2], dsize=(width, height), flags=cv2.INTER_AREA,
+                         borderValue=(128, 128, 128))  # BGR order borderValue
+
+    # Return warped points also
+    if len(targets) > 0:
+        n = targets.shape[0]
+        points = targets[:, 1:5].copy()
+        area0 = (points[:, 2] - points[:, 0]) * (points[:, 3] - points[:, 1])
+
+        # warp points
+        xy = np.ones((n * 4, 3))
+        xy[:, :2] = points[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
+        xy = (xy @ M.T)[:, :2].reshape(n, 8)
+
+        # create new boxes
+        x = xy[:, [0, 2, 4, 6]]
+        y = xy[:, [1, 3, 5, 7]]
+        xy = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+
+        # reject warped points outside of image
+        xy[:, [0, 2]] = xy[:, [0, 2]].clip(0, width)
+        xy[:, [1, 3]] = xy[:, [1, 3]].clip(0, height)
+        w = xy[:, 2] - xy[:, 0]
+        h = xy[:, 3] - xy[:, 1]
+        area = w * h
+        ar = np.maximum(w / (h + 1e-16), h / (w + 1e-16))
+        i = (w > 4) & (h > 4) & (area / (area0 + 1e-16) > 0.1) & (ar < 10)
+
+        targets = targets[i]
+        targets[:, 1:5] = xy[i]
+
+    return imw, targets
+
+
